eda6f89b5b8d9cf3260357dd2ef615cd54ccbc00
Christopher Millan <chrmil11@gmail.com>
2019-02-28 07:28:08 -0600

    Add listener to disable submit upon DOM load

--- site.js
+++ site.js
@@ -1,14 +1,21 @@
 "use strict";
 
 (function () {
+  var interest_submit = document.querySelector('#signup');
+
   // Browser check
   if (!('querySelector' in document && 'addEventListener' in document)) {
+    console.log('querySelector or addEventListener not available');
     return;
   }
 
   // Event Listeners
   // When DOM is loaded, disable submit
-
+  document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOM loaded');
+    interest_submit.setAttribute('disabled', 'disabled');
+    console.log('Submit button disabled');
+  });
 
   // Interactive flair for the form
 
