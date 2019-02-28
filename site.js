48688b909ee0b48d65cd36bbbacc1f2579e955f9
Christopher Millan <chrmil11@gmail.com>
2019-02-28 02:15:01 -0600

    Outline what needs to be done in javascript

---
+++ site.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function () {
+  // Browser check
+
+  // Event Listeners
+  // When DOM is loaded, disable submit
+
+  // Interactive flair for the form
+
+  // Listen whenever the value of either field is changed
+  // If a 'valid' name and email are entered, allow user to submit
+}());
