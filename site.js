426301791564294c83cf98776f5c456e23dc7024
Christopher Millan <chrmil11@gmail.com>
2019-02-28 07:58:28 -0600

    Add temporary value checks for form

--- site.js
+++ site.js
@@ -1,8 +1,6 @@
 "use strict"; // Added b/c of linter. However, validator says to put it inside the function. What's the purpose of this?
 
 (function () {
-  var interest_submit = document.querySelector('#signup');
-
   // Browser check
   if (!('querySelector' in document && 'addEventListener' in document)) {
     console.log('querySelector or addEventListener not available');
@@ -11,14 +9,39 @@
 
   // Event Listeners
   // When DOM is loaded, disable submit
-  document.addEventListener('DOMContentLoaded', function(){
+  document.addEventListener('DOMContentLoaded', function() {
+    var interest_form = document.querySelector('#interestlist');
+    var interest_submit = document.querySelector('#signup');
+    var contact_name = document.querySelector('#name');
+    var contact_email = document.querySelector('#email');
+
     console.log('DOM loaded');
     interest_submit.setAttribute('disabled', 'disabled');
     console.log('Submit button disabled');
+
+    interest_form.addEventListener('keyup', function() {
+      var name_value = contact_name.value;
+      var email_value = contact_email.value;
+      console.log('keyup');
+
+      // Check if both fields are valid
+      if (name_value !== '' && email_value !== '') {
+        // Enable the submit button
+        if (interest_submit.hasAttribute('disabled')) {
+          interest_submit.removeAttribute('disabled');
+          console.log('Submit button enabled');
+        }
+      }
+      else {
+        // Else, keep the submit button disabled
+        if (!interest_submit.hasAttribute('disabled')) {
+          interest_submit.setAttribute('disabled', 'disabled');
+          console.log('Submit button disabled');
+        }
+      }
+
+    });
   });
 
   // Interactive flair for the form
-
-  // Listen whenever the value of either field is changed
-  // If a 'valid' name and email are entered, allow user to submit
 }());
